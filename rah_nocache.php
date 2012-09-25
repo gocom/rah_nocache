369849850635c7846800b44ac280694ddcb79674
Jukka Svahn <web@rahinaa.biz>
2012-09-25 10:45:39 +0300

    Wrap in a class.

--- rah_nocache.php
+++ rah_nocache.php
@@ -13,18 +13,30 @@
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-	if(@txpinterface == 'admin') {
-		rah_nocache();
+	if(txpinterface == 'admin') {
+		new rah_nocache();
 	}
 
-/**
- * Send no-cache headers.
- */
+class rah_nocache {
 
-	function rah_nocache() {
+	/**
+	 * Constructor
+	 */
+
+	public function __construct() {
+		$this->send_headers();
+	}
+
+	/**
+	 * Send no-cache HTTP headers.
+	 */
+
+	public function send_headers() {
 		header('Cache-Control: no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0');
 		header('Expires: Sat, 24 Jul 2003 05:00:00 GMT');
 		header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
 		header('Pragma: no-cache');
 	}
+}
+
 ?>
