0e261ae83c36aceb36d2e8468b0a6a5ec24a52b4
Jukka Svahn <web@rahinaa.biz>
2013-04-17 12:57:37 +0300

    Updated programming style.

--- rah_nocache.php
+++ rah_nocache.php
@@ -13,13 +13,14 @@
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-class rah_nocache {
-
+class rah_nocache
+{
 	/**
 	 * Constructor
 	 */
 
-	public function __construct() {
+	public function __construct()
+	{
 		$this->send_headers();
 	}
 
@@ -27,7 +28,8 @@ class rah_nocache {
 	 * Send no-cache HTTP headers.
 	 */
 
-	public function send_headers() {
+	public function send_headers()
+	{
 		header('Cache-Control: no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0');
 		header('Expires: Sat, 24 Jul 2003 05:00:00 GMT');
 		header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
